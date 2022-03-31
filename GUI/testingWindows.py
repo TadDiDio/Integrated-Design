84cc34ca7b1c1b928705fa34d08100b43f6c9eec
TadDiDio <taddidio@vt.edu>
2022-03-31 12:38:11 -0400

    Initial commit

---
+++ GUI/testingWindows.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk(className=" Bluetooth GUI")
+label = Label(root, text = "Hellow World!").pack()
+
+insultButton = Button(root, bg='#f00')
+insultButton.pack()
+
+
+root.mainloop()
